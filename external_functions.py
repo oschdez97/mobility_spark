5c92d4147011945ffe3c2c075a574330149ec832
oschdez97 <oschdez97@gmail.com>
2021-09-26 07:35:01 -0400

    add user mobility

--- external_functions.py
+++ external_functions.py
@@ -1,5 +1,17 @@
 import numpy as np
 from collections import Counter
+from scipy.spatial import KDTree
+from math import sqrt, pi, sin, cos, acos, atan2
+
+earth_radius = 6371
+
+def get_time_in_seconds(time):
+    '''
+        :param: time str : '16:30'
+        :return int: Number of seconds 59400
+    '''
+    h, m = list(map(int, time.split(':')))
+    return h * 3600 + m * 60
 
 def lower_bound(elems, n):
     low  = 0
@@ -23,6 +35,7 @@ def upper_bound(elems, n):
             high = mid
     return low
 
+# generator
 def get_range(rows, time_init, time_end):
     """
         Get the range of values between a range
@@ -38,12 +51,18 @@ def get_range(rows, time_init, time_end):
     high = upper_bound(times, time_end)
     yield (code, towers[low:high], times[low:high])
 
-def mapp_tow_cell(rows, mapp):
+# indices of the interval
+def get_range_interval(times, time_init, time_end):
+    low  = lower_bound(times, time_init)
+    high = upper_bound(times, time_end)
+    return [low, high]
+
+def mapp_cell_tow(rows, mapp):
     """
-        Map from tower to cell
-        :param list rows: The list of imsis, towers and times        
-        :param mapp dict: The dictionary tow -> cell
-        :return: The list of imsis, cells and times 
+        Map from cell to tower(area_correlator)
+        :param list rows: The list of imsis, cells and times        
+        :param mapp dict: The dictionary cell -> tow
+        :return: The list of imsis, towers and times 
     """
     cells  = []
     times  = []
@@ -53,7 +72,6 @@ def mapp_tow_cell(rows, mapp):
             cells.append(mapp[_id])
             times.append(_time)
     yield (rows[0], cells, times)
-
 
 def count_occurrences_and_normalize(elems):
     count = dict(Counter(elems))
@@ -70,9 +88,138 @@ def flat_origin_destination_product(row):
             yield (cell_start, cell_end, float(val_1) * float(val_2))
 
 
+def distance_displacement(row, mapp):
+    tower_start = row[0]
+    tower_end = row[1]
+    product = row[2]
+
+    tower_start_coords = mapp[tower_start]
+    tower_end_coords = mapp[tower_end]
+    distance = distance_in_km_between_coordinates(tower_start_coords, tower_end_coords)
+    
+    yield (row[0], distance * product)
+
+
 def fix_json_format(jobj):
     m = {}
     for i in jobj:
         m[jobj[i]['start']] = jobj[i]
         del m[jobj[i]['start']]['start']
     return m
+
+
+'''
+aqui acumular el tiempo por torre
+'''
+
+def accumulate_count(data, lower_bound, upper_bound):
+    code = data[0]
+    towers = data[1]
+    times = data[2]
+    t_res = {}
+
+    for i, tow in enumerate(towers):
+        if lower_bound < times[i] < upper_bound:
+            if tow not in t_res:
+                t_res[tow] = [1, times[i]]
+            else:
+                t_res[tow][0] += 1
+                t_res[tow][1] += times[i]
+
+    count_towers = []
+    sum_of_time_by_tower = []    
+    for i in list(t_res.values()):
+        count_towers.append(i[0])
+        sum_of_time_by_tower.append(i[1])
+
+    yield (code, list(t_res.keys()), count_towers, sum_of_time_by_tower)
+
+def map_area_correlator_to_coord(towers, mapp):
+    res = []
+    for i in towers:
+        if mapp.__contains__(i):
+            res.append(mapp[i])
+    return res
+
+def map_coord_to_area_correlator(coord, mapp):
+    coord = str(coord)
+    if mapp.__contains__(coord):
+        return mapp[coord]
+
+def get_mean_home_tower(weights, coords):
+    mean_tower = [0, 0]
+    weights = list(map(float, weights))
+    coords = [list(map(float, coord)) for coord in coords]
+    for i in range(len(coords)):
+        # mean_tower = mean_tower + weights[i] * tower_coord
+        # getting trouble with numpy and udf
+        tower_coord = coords[i]
+        tmp = [weights[i] * tower_coord[0], weights[i] * tower_coord[1]]
+        mean_tower = [mean_tower[0] + tmp[0], mean_tower[1] + tmp[1]]
+    kdtree = KDTree(coords)
+    kdtree_query = kdtree.query(mean_tower, 1)
+    res = list(map(str, list(kdtree.data[kdtree_query[1]])))
+    return res
+
+def degrees_to_radians(degrees):
+	return degrees * pi / 180
+
+def distance_in_km_between_coordinates(coord1, coord2):
+	lat1, lon1 = list(map(float, coord1))
+	lat2, lon2 = list(map(float, coord2))
+    
+	dLat = degrees_to_radians(lat2 - lat1)
+	dLon = degrees_to_radians(lon2 - lon1)
+
+	lat1 = degrees_to_radians(lat1)
+	lat2 = degrees_to_radians(lat2)
+
+	a = sin(dLat / 2) ** 2 + sin(dLon / 2) ** 2 * cos(lat1) * cos(lat2)
+	c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+	return earth_radius * c
+
+def km_displacement(elems):
+    res = []
+    for i in range(len(elems)-1):
+        d = distance_in_km_between_coordinates(elems[i], elems[i+1])
+        res.append(d)
+    return sum(res)
+
+def between_ab_OR_dc(data, interval_1, interval_2):
+    code   = data[0]
+    towers = data[1]
+    times  = data[2]
+    interval_1 = get_range_interval(times, interval_1[0], interval_1[1])
+    interval_2 = get_range_interval(times, interval_2[0], interval_2[1])
+    zip_data = list(zip(times, towers))
+    a = zip_data[interval_1[0] : interval_1[0] + interval_1[1]]
+    b = zip_data[interval_2[0] : interval_2[0] + interval_2[1]]
+    res = list(set(a) | set(b))
+    res.sort()
+    towers = [x[1] for x in res]
+    times  = [x[0] for x in res]
+    yield (code, towers, times)
+
+def get_spent_time(times):
+    last_time = 24 * 60 * 60
+    res = [0] * len(times)
+    for i in range(0, len(times) - 1):
+        current_register = times[i]
+        next_register = times[i + 1]
+        delta = abs(next_register - current_register)
+        half_expended_time = delta / 2
+        res[i] += half_expended_time
+        res[i+1] += half_expended_time
+        #res[i] = delta
+    res[-1] += abs(last_time - times[-1])
+
+    # normalize
+    normalized = [val / last_time for val in res]
+    return normalized
+
+def distance_to(home_tower, towers):
+    res = []
+    for tower in towers:
+        res.append(distance_in_km_between_coordinates(home_tower, tower)) 
+    return res
--- query_script_spark.py
+++ query_script_spark.py
@@ -1,5 +1,6 @@
 import json
 import time
+import math
 import numpy as np
 import pandas as pd
 import pyspark.sql.functions as F
@@ -11,14 +12,17 @@ from scipy.spatial import KDTree
 from external_functions import *
 
 
+CURRENT_DIR  = '/home/hellscream/Documents/backendSpark'
 CELL_AREA    = '/data/cell_area.csv'
 PARQUETS_DIR = '/data/parquets'
-CURRENT_DIR  = '/home/hellscream/Documents/backendSpark'
+TOWER_AUX_PROPERTIES_FILE = CURRENT_DIR + '/data/voronoi_props.json'
 
-with open('tow-cell.json') as json_file:
-    tow_cell_mapp = json.load(json_file)
+with open('cell-tow.json') as json_file:
+    cell_tow_mapp = json.load(json_file)
 
-mapps = {}
+with open('tow-latlon.json') as json_file:
+    tow_latlon_mapp = json.load(json_file)
+
 
 '''
 root
@@ -29,55 +33,39 @@ root
  |    |-- element: long (containsNull = true)
 '''
 
+# do not mistake
+# tower : dhj7w9
+# cell  : 171-28271
+
 def init_spark_session(name='Mobility'):
     spark = SparkSession.builder.appName(name).getOrCreate()
-    #calculate_static_mappings(spark)
     return spark
 
-def calculate_static_mappings(spark):
-    global mapps
-    
-    dic_full_df = spark.read.format('csv').options(header='true', delimiter='\t')\
-                    .load(CURRENT_DIR + CELL_AREA)\
-                    .select('*')
-                    
-    dic_full_pandas = dic_full_df.toPandas()
-    dic_full = dic_full_pandas.to_dict('index')
-    
-    dic_tow_cell = {}
-    for i in dic_full:
-        dic_tow_cell[dic_full[i]['id']] = dic_full[i]['area_correlator'] 
-    
-    dic_cell_latlon = {}
-    for i in dic_full:
-        dic_cell_latlon[dic_full[i]['area_correlator']] = [dic_full[i]['latitude'], dic_full[i]['longitude']]
 
-    mapps['tow-cell'] = dic_tow_cell
-    mapps['cell-latlon'] = dic_cell_latlon
-
-    with open('tow-cell.json', 'w') as outfile:
-        json.dump(dic_tow_cell, outfile)
-
-
-    with open('cell-latlon.json', 'w') as outfile:
-        json.dump(dic_cell_latlon, outfile)
-
+map_area_correlator_to_coord_udf = F.udf(lambda x : map_area_correlator_to_coord(x, tow_latlon_mapp)\
+        ,ArrayType(ArrayType(StringType())))
 
 ######################################################################
 ############################## Mobility ##############################
 ######################################################################
 
 def mobility(spark_session, date, time_start_lower, time_start_high, time_end_lower, time_end_high):
-    users_cells_start = get_mobility_at_time_interval(spark_session, date, time_start_lower, time_start_high) 
-    users_cells_end = get_mobility_at_time_interval(spark_session, date, time_end_lower, time_end_high)
-    matrix = build_matrix(date, users_cells_start, users_cells_end)
+    
+    users_cells_start, workers = get_mobility_at_time_interval(spark_session, date, time_start_lower, time_start_high) 
+    users_cells_end, sleepers = get_mobility_at_time_interval(spark_session, date, time_end_lower, time_end_high)
+    sleepers_v2 = get_sleepers(spark_session, date)
+    matrix, vector = build_matrix(date, users_cells_start, users_cells_end, workers, sleepers, sleepers_v2)
 
-    with open('2021-03-01.json', 'w') as file:
+    with open('2021-03-01_matrix.json', 'w') as file:
         json.dump(matrix, file)
 
+    with open('2021-03-01_vector.json', 'w') as file:
+        json.dump(vector, file)
+
+
 def get_mobility_at_time_interval(spark, date, time_start, time_end):
-    time_start *= 3600
-    time_end *= 3600
+    time_start = get_time_in_seconds(time_start)
+    time_end = get_time_in_seconds(time_end)
 
     # load correspondent parquet
     df = spark.read.parquet(CURRENT_DIR + PARQUETS_DIR + '/' + date)
@@ -86,17 +74,59 @@ def get_mobility_at_time_interval(spark, date, time_start, time_end):
     df = df.rdd.flatMap(lambda rows : get_range(rows, time_start, time_end))\
             .toDF(['code', 'towers', 'times']).select('*').filter(F.size(F.col('times')) > 0)
 
-    # map tower_id to cell_id
-    df = df.rdd.flatMap(lambda x : mapp_tow_cell(x, tow_cell_mapp)).toDF(['code', 'towers', 'times'])
+    # map cell_id to tower_id
+    df = df.rdd.flatMap(lambda x : mapp_cell_tow(x, cell_tow_mapp)).toDF(['code', 'towers', 'times'])
 
     # count distinct ocurrences of every cell and normalize
     count_occurrences_udf = F.udf(lambda x : count_occurrences_and_normalize(x), ArrayType(ArrayType(StringType())))
     df = df.select('code', count_occurrences_udf(F.col('towers')).alias('towers-count'))
 
-    return df
+    # tower -> count: workers/sleepers depending of the given role 
+    user_tower_role = df.rdd.flatMap(lambda row : ((tow, val) for tow, val in row[1])).toDF(['tower', 'val'])
+    user_tower_role = user_tower_role.groupBy('tower').agg(F.sum('val').alias('sum')).toPandas().to_dict(orient='list')
+    user_tower_role = dict(zip(user_tower_role['tower'], user_tower_role['sum']))
+
+    return df, user_tower_role
 
 
-def build_matrix(date, users_cells_start, users_cells_end):
+def get_sleepers(spark, date, time_sleep_lower='01:00', time_sleep_high='04:00'):
+    time_sleep_lower = get_time_in_seconds(time_sleep_lower)
+    time_sleep_high = get_time_in_seconds(time_sleep_high)
+    
+    # load correspondent parquet
+    df = spark.read.parquet(CURRENT_DIR + PARQUETS_DIR + '/' + date)
+
+    # map cell_id to tower_id
+    users_cells_sleep = df.rdd.flatMap(lambda x : mapp_cell_tow(x, cell_tow_mapp)).toDF(['code', 'towers', 'times'])
+    
+    # count distinct ocurrences of every cell
+    users_cells_sleep = users_cells_sleep.rdd.flatMap(lambda x : accumulate_count(x, time_sleep_lower, time_sleep_high))\
+        .toDF(['code', 'towers', 'count'])
+    users_cells_sleep = users_cells_sleep.select('*').where(F.size('towers') > 0)
+
+    #map cell_id to latlon coords
+    #map_area_correlator_to_coord_udf = F.udf(lambda x : map_area_correlator_to_coord(x, tow_latlon_mapp), ArrayType(ArrayType(StringType())))
+    users_cells_sleep = users_cells_sleep.withColumn('coords', map_area_correlator_to_coord_udf(F.col('towers'))).select('*')
+
+    # getting mean and home tower
+    normalize_udf = F.udf(lambda values : [val / sum(values) for val in values], ArrayType(StringType()))
+    mean_home_tower_udf = F.udf(lambda weight, coords : get_mean_home_tower(weight, coords), ArrayType(StringType()))
+    
+    # imsi -> spleep_area
+    users_cells_sleep = users_cells_sleep.withColumn('weight', normalize_udf(F.col('count')))\
+                                .select('code', mean_home_tower_udf(F.col('weight'), F.col('coords')).alias('home_tower'))
+    
+    # area_correlator -> count
+    coord_area_correlator_mapp = {str(v): k for k, v in tow_latlon_mapp.items()}
+    map_coord_to_area_correlator_udf = F.udf(lambda x : map_coord_to_area_correlator(x, coord_area_correlator_mapp), StringType())
+    res = users_cells_sleep.select(map_coord_to_area_correlator_udf(F.col('home_tower')).alias('area_correlator'))
+    res = res.groupBy('area_correlator').count()
+    
+    res = res.toPandas().to_dict(orient='list')
+    return dict(zip(res['area_correlator'], res['count']))
+    
+
+def build_matrix(date, users_cells_start, users_cells_end, workers, sleepers, sleepers_v2):
     union_users_cells = users_cells_start.union(users_cells_end)
     
     union_users_cells = union_users_cells.groupBy('code')\
@@ -105,27 +135,214 @@ def build_matrix(date, users_cells_start, users_cells_end):
 
     rdd = union_users_cells.select('cells').rdd.flatMap(lambda x : flat_origin_destination_product(x))
     df = rdd.toDF(['start', 'end', 'value'])
+
+    tower_user_displacement_distance = df.rdd.flatMap(lambda row : distance_displacement(row, tow_latlon_mapp)).toDF(['start', 'val'])
+    tower_user_displacement_distance = tower_user_displacement_distance.groupBy('start').count()
+    tower_user_displacement_distance = tower_user_displacement_distance.toPandas().to_dict(orient='list')
+    tower_user_displacement_distance = dict(zip(tower_user_displacement_distance['start'],\
+        tower_user_displacement_distance['count']))
+
     df = df.groupBy('start').pivot('end').agg(F.sum('value')).na.fill(value=0)
 
     matriz_pandas = df.toPandas()
-    
     m = fix_json_format(json.loads(matriz_pandas.to_json(orient='index')))
     m_pandas = pd.DataFrame(m)
     m_transpose = m_pandas.transpose().to_dict()
 
+    fd = open(TOWER_AUX_PROPERTIES_FILE, 'r')
+    aux_properties = json.loads(fd.read())
+    aux_properties = aux_properties['features']
+    tower_aux_properties = {}
+    for feature in aux_properties:
+        tower = feature['properties']['id']
+        pop = feature['properties']['pop']
+        area = feature['properties']['area']
+        tower_aux_properties[tower] = {'area': area, 'pop': pop}
+
+    vector_data = {}
+    tower_sleeper = set(sleepers.keys())
+    tower_sleeper_v2 = set(sleepers_v2.keys())
+    tower_worker = set(workers.keys())
+
+    for tower in (tower_sleeper | tower_worker | tower_sleeper_v2):
+        if tower not in vector_data:
+            vector_data[tower] = []
+        if tower in tower_sleeper:
+            vector_data[tower].append(sleepers[tower])
+        else:
+            vector_data[tower].append(0)
+        if tower in tower_sleeper_v2:
+            vector_data[tower].append(sleepers_v2[tower])
+        else:
+            vector_data[tower].append(0)
+        if tower in tower_worker:
+            vector_data[tower].append(workers[tower])
+        else:
+            vector_data[tower].append(0)
+        
+        if tower in tower_user_displacement_distance and tower in sleepers:
+            vector_data[tower].append(tower_user_displacement_distance[tower] / sleepers[tower])
+        else:
+            vector_data[tower].append(0)
+        if tower in tower_aux_properties:
+            area = tower_aux_properties[tower]['area']
+            pop = tower_aux_properties[tower]['pop']
+            vector_data[tower].append(area)
+            vector_data[tower].append(pop)
+        else:
+            vector_data[tower].append(0)
+            vector_data[tower].append(0)
+    
+    
     matrix = {
 			'type': 'matrix',
 			'labels': ['interaction'],
 			'date': date,
 			'matrix': m,
 			'matrixTranspose': m_transpose,
-		}
+	}
+
+    vector = {
+			'type': 'vector',
+			'labels': ['sleeper', 'sleeper2',
+			           'workers', 'mobility',
+			           'area', 'pop'],
+			'date': date,
+			'data': vector_data
+	}
+
+    return matrix, vector
+
+
+##########################################################################
+############################## UserMobility ##############################
+##########################################################################
+
+get_distance_to_udf = F.udf(lambda home_tower, towers : distance_to(home_tower, towers), ArrayType(StringType()))
+
+def radius_gyration(distance, weights):
+    distance = list(map(float, distance))
+    weights = list(map(float, weights))
+    res = [weights[i] * distance[i] ** 2 for i in range(len(distance))]
+    return math.sqrt(sum(res))
+
+def get_radius_gyration(df):
+    # get the time spent in each tower and normalize
+    get_spent_time_udf = F.udf(lambda x : get_spent_time(x), ArrayType(StringType()))
+    df = df.select('code', 'home_tower', 'towers', 'coords', get_spent_time_udf(F.col('acum_time')).alias('weights'))
+
+    # calculate the distance from home tower to each tower register
+    df = df.select('code', get_distance_to_udf(F.col('home_tower'), F.col('coords')).alias('distance'), 'weights')
+
+    # calculate radius_gyration
+    radius_gyration_udf = F.udf(lambda weights, distances : radius_gyration(distances, weights))
+    df = df.select('code', radius_gyration_udf(F.col('weights'), F.col('distance')).alias('radius_gyration'))
+
+    return df
+
+def farthest_tower(towers, distances):
+    distances = list(map(float, distances))
+    tower = None
+    max_distance = -10e8
+    for i, d in enumerate(distances):
+        if d > max_distance:
+            max_distance = d
+            tower = towers[i]
+    return tower
+
+def get_users_mobility(spark, date, start_time='07:00', end_time='20:00', sleep_start_time='01:00', sleep_end_time='04:00'):
+    start_time = get_time_in_seconds(start_time)
+    end_time = get_time_in_seconds(end_time)
+    sleep_start_time = get_time_in_seconds(sleep_start_time)
+    sleep_end_time = get_time_in_seconds(sleep_end_time)
     
-    return matrix
+    df = spark.read.parquet(CURRENT_DIR + PARQUETS_DIR + '/' + date)
+    
+    # records between (start_time, end_time) OR (sleep_start_time, sleep_end_time)
+    df = df.rdd.flatMap(lambda rows : between_ab_OR_dc(rows, (start_time, end_time), (sleep_start_time, sleep_end_time)))\
+        .toDF(['code', 'cell_ids', 'times'])
+    df = df.select('*').where(F.size(df.times) > 0)
+    
+    # imsi -> cell changes & amount of distinct cell
+    distinct_cells_df = df.withColumn('distinct_cells', F.array_distinct(F.col('cell_ids')))\
+        .select('code', 'distinct_cells', F.size(F.col('distinct_cells')).alias('distinct_cells_count'))
+    
+    # map cell -> tow
+    df = df.rdd.flatMap(lambda x : mapp_cell_tow(x, cell_tow_mapp)).toDF(['code', 'towers', 'times'])
+
+    # imsi -> km displacement
+    km_displacement_df = df.select('code', 'towers', map_area_correlator_to_coord_udf(F.col('towers')).alias('lat_lon'))
+    km_displacement_udf = F.udf(lambda coordinates : km_displacement(coordinates), StringType())
+    km_displacement_df = km_displacement_df.withColumn('km_displacement', km_displacement_udf(F.col('lat_lon')))\
+        .select('code', 'km_displacement')
+    
+    # imsi -> amount of distinct towers
+    distinct_towers_df = df.withColumn('distinct_towers', F.array_distinct(F.col('towers'))).\
+        select('code', 'distinct_towers', F.size(F.col('distinct_towers')).alias('distinct_towers_count')) 
+    
+    # process for establish sleeping zone
+    users_cells_start = df.rdd.flatMap(lambda x : accumulate_count(x, sleep_start_time, sleep_end_time)).\
+        toDF(['code', 'towers', 'count', 'acum_time'])
+
+    users_cells_start = users_cells_start.select('*').where(F.size('towers') > 0)
+    users_cells_start = users_cells_start.withColumn('coords', map_area_correlator_to_coord_udf(F.col('towers'))).select('*')
+
+    # getting mean and home tower
+    normalize_udf = F.udf(lambda values : [val / sum(values) for val in values], ArrayType(StringType()))
+    mean_home_tower_udf = F.udf(lambda weight, coords : get_mean_home_tower(weight, coords), ArrayType(StringType()))
+    
+    # imsi -> spleep_area
+    users_cells_start = users_cells_start.withColumn('weight', normalize_udf(F.col('count')))\
+        .select('code', mean_home_tower_udf(F.col('weight'), F.col('coords')).alias('home_tower'), 
+        'towers', 'coords', 'acum_time')
+    
+    # radius gyration
+    radius_gyration_df = get_radius_gyration(users_cells_start)
+
+    # obtain farthest tower visited
+    farthest_tower_df = users_cells_start.select('code', 'towers', \
+        get_distance_to_udf(F.col('home_tower'), F.col('coords')).alias('distance'))
+    
+    get_farthest_tower_udf = F.udf(lambda towers, distances : farthest_tower(towers, distances))
+    farthest_tower_df = farthest_tower_df.select('code', \
+        get_farthest_tower_udf(F.col('towers'), F.col('distance')).alias('farthest_tower'))
+    
+    # remove unnecessary columns
+    users_cells_start = users_cells_start.select('code', 'home_tower')
+    distinct_cells_df = distinct_cells_df.select('code', 'distinct_cells_count')
+    distinct_towers_df = distinct_towers_df.select('code', 'distinct_towers_count')
+
+    results_dfs = [users_cells_start, distinct_cells_df, km_displacement_df, distinct_towers_df, \
+        radius_gyration_df, farthest_tower_df] 
+
+    # return users_cells_start, distinct_cells_df, km_displacement_df, distinct_towers_df, radius_gyration_df, farthest_tower_df
+
+    user_mobility = users_cells_start.unionByName(distinct_cells_df, allowMissingColumns=True)
+    for df in results_dfs[2:]:     
+        user_mobility = user_mobility.unionByName(df, allowMissingColumns=True)
+    return user_mobility 
+    #user_mobility.groupBy('code').agg(F.sum('distinct_cells_count'))
+
 
 def test_mobility_matrix():
+    '''
+        time sent 1914.185616493225s = 31m
+    '''
+    start = time.time()
     spark_session = init_spark_session()
-    mobility(spark_session, '2021-03-01', 6, 10, 16, 20)
+    mobility(spark_session, '2021-03-01', '06:00', '10:00', '16:00', '20:00')
+    end = time.time()
+    print('time spent: ' + str((end - start) / 60))
+
+
+def test_user_mobility():
+    spark_session = init_spark_session()
+    user_mobility = get_users_mobility(spark_session, '2021-03-01s')
+    user_mobility.show()
+    user_mobility.printSchema()
 
 if __name__ == '__main__':
-    test_mobility_matrix()
+    test_user_mobility()
+    # test_mobility_matrix()
+
+
